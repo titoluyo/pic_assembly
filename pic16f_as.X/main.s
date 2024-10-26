c96abb32839da0f8c8342d2d714d674235324718
Tito Luyo Murata <tluyo@hotmail.com>
2024-10-26 16:08:42 -0500

    pwm

--- pic16f_as.X/main.s
+++ pic16f_as.X/main.s
@@ -2,6 +2,7 @@
 #include "common.inc"
 #include "led.inc"
 #include "lcd_8bits.inc"
+#include "pwm.inc"
 
 ; CONFIG
   CONFIG  FOSC = XT             ; Oscillator Selection bits (HS oscillator)
@@ -20,18 +21,19 @@ PROGRAMA:
     ;goto    INICIO_LCD
     ;goto    MAIN_LED
     ;goto    MAIN_LCD
-    goto    LCD_INIT
+    ;goto    LCD_INIT
+    goto    PWM_INIT
     
     psect code
 BLINK:
     bank1
-    clrf    TRISA           ; Salida
-    clrf    TRISD           ; Salida
-    clrf    TRISC           ; Salida
+    clrf    TRISA           ; Output
+    clrf    TRISD           ; Output
+    clrf    TRISC           ; Output
     bank0
     bcf	    PORTA, 3	    ; RA3=0
     bsf	    PORTC, 5; RC5=1
-    ;call    Retardo_1ms     ; Espera 1 ms
+    ;call    Delay_1ms     ; Wait 1 ms
 BLINK_LOOP:
     bsf	    PORTD, 0
     call    Delay2
@@ -39,33 +41,7 @@ BLINK_LOOP:
     call    Delay2
     goto BLINK_LOOP
     
-/*
-    psect code
-INICIO_LCD:
-	CALL	LCD_INIT
-	MOVLW	1
-	CALL	LCD_LINEA1
-	MOVLW	'H'
-	CALL	LCD_CARACTER
-	MOVLW	'O'
-	CALL	LCD_CARACTER
-	MOVLW	'L'
-	CALL	LCD_CARACTER
-	MOVLW	'A'
-	CALL	LCD_CARACTER
-	MOVLW	5
-	CALL	LCD_LINEA2
-	MOVLW	'W'
-	CALL	LCD_CARACTER
-	MOVLW	'E'
-	CALL	LCD_CARACTER
-	MOVLW	'L'
-	CALL	LCD_CARACTER
-	MOVLW	'S'
-	CALL	LCD_CARACTER
-START_LCD:
-	GOTO	START_LCD
-*/
+
 	
 	
     end			    ; End of the program
