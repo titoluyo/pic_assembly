15ede19ff5c51d16c8072ea27944791b820aa61f
Tito Luyo Murata <tluyo@hotmail.com>
2024-10-25 22:34:37 -0500

    lcd 8bits

--- pic16f_as.X/main.s
+++ pic16f_as.X/main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 #include "common.inc"
 #include "led.inc"
-#include "lcd_4bits.inc"
+#include "lcd_8bits.inc"
 
 ; CONFIG
   CONFIG  FOSC = XT             ; Oscillator Selection bits (HS oscillator)
@@ -16,9 +16,59 @@
 
     psect programa, class = CODE, reloc=2
 PROGRAMA:
-    goto    MAIN_LED
-
-    end                  ; End of the program
+    ;goto    BLINK
+    ;goto    INICIO_LCD
+    ;goto    MAIN_LED
+    ;goto    MAIN_LCD
+    goto    LCD_INIT
+    
+    psect code
+BLINK:
+    bank1
+    clrf    TRISA           ; Salida
+    clrf    TRISD           ; Salida
+    clrf    TRISC           ; Salida
+    bank0
+    bcf	    PORTA, 3	    ; RA3=0
+    bsf	    PORTC, 5; RC5=1
+    ;call    Retardo_1ms     ; Espera 1 ms
+BLINK_LOOP:
+    bsf	    PORTD, 0
+    call    Delay2
+    bcf	    PORTD, 0
+    call    Delay2
+    goto BLINK_LOOP
+    
+/*
+    psect code
+INICIO_LCD:
+	CALL	LCD_INIT
+	MOVLW	1
+	CALL	LCD_LINEA1
+	MOVLW	'H'
+	CALL	LCD_CARACTER
+	MOVLW	'O'
+	CALL	LCD_CARACTER
+	MOVLW	'L'
+	CALL	LCD_CARACTER
+	MOVLW	'A'
+	CALL	LCD_CARACTER
+	MOVLW	5
+	CALL	LCD_LINEA2
+	MOVLW	'W'
+	CALL	LCD_CARACTER
+	MOVLW	'E'
+	CALL	LCD_CARACTER
+	MOVLW	'L'
+	CALL	LCD_CARACTER
+	MOVLW	'S'
+	CALL	LCD_CARACTER
+START_LCD:
+	GOTO	START_LCD
+*/
+	
+	
+    end			    ; End of the program
 
 
 
